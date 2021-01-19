88691351cb7662c10ed19f316a62d548f8d8a85f
uu64 <yusuke.t.0808@gmail.com>
2021-01-19 22:40:09 +0900

    add: handler for line message

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/uu64/line-bot-roomba
 
 go 1.14
 
-require github.com/line/line-bot-sdk-go v7.7.0+incompatible // indirect
+require (
+	cloud.google.com/go/logging v1.1.2 // indirect
+	github.com/line/line-bot-sdk-go v7.7.0+incompatible
+)
--- main.go
+++ main.go
@@ -1,23 +1,63 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
+var bot *linebot.Client
+
+func init() {
+	token := os.Getenv("LINE_BOT_TOKEN")
+	secret := os.Getenv("LINE_BOT_SECRET")
+
+	var err error
+	bot, err = linebot.New(secret, token)
+	if err != nil {
+		// TODO: check log location
+		log.Fatal(err)
+	}
+}
+
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/bot/webhook" {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello, World!")
+
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if strings.Contains(message.Text, "ルンちゃん") {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("ほい")).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	}
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
+	// initialize handler
+	http.HandleFunc("/bot/webhook", webhookHandler)
 
+	// initialize server
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
