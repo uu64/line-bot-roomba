00d94f12489236e5171342aa7ca2a84627ff1d5f
uu64 <yusuke.t.0808@gmail.com>
2021-01-20 23:36:03 +0900

    add: ifttt handler

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +13,10 @@ import (
 
 var bot *linebot.Client
 
+type iftttReqBody struct {
+	Event string `json:event`
+}
+
 func init() {
 	token := os.Getenv("LINE_BOT_TOKEN")
 	secret := os.Getenv("LINE_BOT_SECRET")
@@ -20,6 +26,42 @@ func init() {
 	if err != nil {
 		// TODO: check log location
 		log.Fatal(err)
+	}
+}
+
+func iftttHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/bot/ifttt" {
+		http.NotFound(w, r)
+		return
+	}
+
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if r.Header.Get("Content-Type") != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var body iftttReqBody
+	if err = json.Unmarshal(b, &body); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if body.Event == "start-cleaning" {
+		if _, err := bot.BroadcastMessage(linebot.NewTextMessage("掃除おわった")).Do(); err != nil {
+			log.Print(err)
+		}
 	}
 }
 
@@ -56,6 +98,7 @@ func webhookHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	// initialize handler
 	http.HandleFunc("/bot/webhook", webhookHandler)
+	http.HandleFunc("/bot/ifttt", iftttHandler)
 
 	// initialize server
 	port := os.Getenv("PORT")
